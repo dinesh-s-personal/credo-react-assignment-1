03f7a0bb8ca2dd211550126dbc2e1e87686c7249
dineshs-toast <86337790+dineshs-toast@users.noreply.github.com>
2024-05-14 20:24:18 +0530

    Updated code to remove build error

--- src/components/Assignment-useEffect.jsx
+++ src/components/Assignment-useEffect.jsx
@@ -7,7 +7,7 @@ export const AssignUseEffect = () => {
     const stateIndia = ['Delhi','Tamil Nadu','Kerala'];
     const stateSrilnka = ['Norther Srilanka','Dumbela'];
     const [stateList, setStateList] = React.useState([]);
-    const [selCountry, setSelCountry] = React.useState('');
+    const [selCountry, setSelCountry] = React.useState('India');
 
     const handleCountryChange = (e) => {
         setSelCountry(e.target.value);
@@ -30,7 +30,7 @@ export const AssignUseEffect = () => {
     return <div>
         <h1>Assignment Task 8 - Contry and State details using useEffect</h1>
 
-        <label for="country" style={{margin: "5px"}}>Contry:</label>
+        <label style={{margin: "5px"}}>Contry:</label>
         <select id="country"
                 name="country"
                 value={selCountry}
@@ -40,7 +40,7 @@ export const AssignUseEffect = () => {
                 ))}
         </select>
         <br></br>
-        <label for="state" style={{margin: "5px"}}>State:</label>
+        <label style={{margin: "5px"}}>State:</label>
         <select id="state"
                 name="state">
             {stateList.map((item, index) => (
